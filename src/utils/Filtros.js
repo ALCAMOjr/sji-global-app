f5d1d588f372fa221057e9db43781f4c68ed13e0
Alfredo <alfredocastellanoula.14@gmail.com>
2024-08-27 11:33:04 -0600

    Filtros

--- src/utils/Filtros.js
+++ src/utils/Filtros.js
@@ -1,14 +1,18 @@
 export const filtros = [
-    { 
-      name: 'RADICACIONES/ADMISION/NO NOTIFICADA/TERMINADA', 
-      value: { etapa: 'RADICACIONES', termino: 'ADMISION', notificacion: 'NO NOTIFICADA/TERMINADA' } 
-    },
-    { 
-      name: 'RADICACIONES/ADMISION/SOLICITADA', 
-      value: { etapa: 'RADICACIONES', termino: 'ADMISION', notificacion: 'SOLICITADA' } 
-    },
-    { 
-      name: 'RADICACIONES/ADMISION/NOTIFICADA/TERMINADA', 
-      value: { etapa: 'RADICACIONES', termino: 'ADMISION', notificacion: 'NOTIFICADA/TERMINADA' } 
-    }
-  ];
+  {
+    name: 'RADICACIONES/DESECHAMIENTO/',
+    value: { etapa: 'RADICACIONES', termino: 'DESECHAMIENTO', notificacion: '' }
+  },
+  {
+    name: 'RADICACIONES/ADMISION/NO NOTIFICADA/TERMINADA',
+    value: { etapa: 'RADICACIONES', termino: 'ADMISION', notificacion: 'NO NOTIFICADA/TERMINADA' }
+  },
+  {
+    name: 'RADICACIONES/ADMISION/SOLICITADA',
+    value: { etapa: 'RADICACIONES', termino: 'ADMISION', notificacion: 'SOLICITADA' }
+  },
+  {
+    name: 'RADICACIONES/ADMISION/NOTIFICADA/TERMINADA',
+    value: { etapa: 'RADICACIONES', termino: 'ADMISION', notificacion: 'NOTIFICADA/TERMINADA' }
+  }
+];

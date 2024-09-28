dea93225a3501f09f490af5ab9ee53bb1666a33c
danielaestefania19 <danielacastellano1000@gmail.com>
2024-09-28 17:50:25 -0600

    Modal

--- src/components/Expediente/Expedientes.jsx
+++ src/components/Expediente/Expedientes.jsx
@@ -47,7 +47,7 @@ const Expedientes = () => {
     const [isSubmitDisabled, setIsSubmitDisabled] = useState(false);
     const [IsLoadingUpdateAllExpedientes, setIsLoadingUpdateAllExpedientes] = useState(false)
     const [progress, setProgress] = useState(0);
-    
+
 
 
     const openModalUpload = () => {
@@ -144,7 +144,7 @@ const Expedientes = () => {
 
         setCurrentExpedientes(reversedExpedientes.slice(startIndex, endIndex));
     }, [expedientes, itemsPerPage, currentPage]);
-    
+
 
     const handleChangePage = (event, newPage) => {
         setCurrentPage(newPage + 1);
@@ -243,7 +243,7 @@ const Expedientes = () => {
             let progress = 0;
             let result = null;
             let state = '';
-    
+
             while (progress < 100) {
                 const response = await getExpedienteJobStatus({ token: jwt, jobId });
                 state = response.state;
@@ -254,31 +254,31 @@ const Expedientes = () => {
                     return { success: true, result };
                 } else if (state === 'failed') {
                     result = response.result;
-                        return { success: false, error: "Something was wrong." };
+                    return { success: false, error: "Something was wrong." };
                 }
-    
+
                 await new Promise(resolve => setTimeout(resolve, 1000));
             }
-    
+
             return { success: false, error: 'Something was wrong.' };
-    
+
         } catch (error) {
             return { success: false, error: error.message };
         }
     };
-    
-    
+
+
     const handleUpdateAllExpedientes = async (e) => {
         e.preventDefault();
         setIsLoadingUpdateAllExpedientes(true);
         setProgress(0);
-    
+
         try {
             const { success, jobId } = await UpdateAllExpedientes(setOriginalExpedientes);
-    
+
             if (success) {
                 const { success: monitorSuccess, result, error } = await monitorJobProgress(jobId);
-    
+
                 if (monitorSuccess) {
                     toast.info('Se le notificó por correo electrónico con el resultado del proceso.', {
                         icon: () => <img src={check} alt="Success Icon" />,
@@ -294,7 +294,8 @@ const Expedientes = () => {
                             progressStyle: {
                                 background: '#1D4ED8',
                             }
-                        });   }
+                        });
+                    }
                 }
             }
         } catch (error) {
@@ -304,8 +305,8 @@ const Expedientes = () => {
             setIsLoadingUpdateAllExpedientes(false);
         }
     };
-    
-    
+
+
 
 
     const handleCreate = async (e) => {
@@ -336,8 +337,8 @@ const Expedientes = () => {
                     toast.error('La URL proporcionada es incorrecta. Intente de nuevo.');
                 }
                 else if (error === 'Tribunal no funciono') {
-                toast.error('El Tribunal Virtual fallo al devolver los datos. Intente de nuevo.');
-            }  
+                    toast.error('El Tribunal Virtual fallo al devolver los datos. Intente de nuevo.');
+                }
                 else {
                     toast.error('Algo mal sucedió al crear el expediente: ' + error);
                 }
@@ -377,13 +378,13 @@ const Expedientes = () => {
     const handleUpdate = async (e) => {
         e.preventDefault();
         setIsLoading(true);
-    
+
         if (!formData.url) {
             toast.error('La URL no existe. Por favor, agrega una URL e intenta de nuevo.');
             setIsLoading(false);
-            return; 
+            return;
         }
-    
+
         try {
             const { success, error } = await updateExpediente({
                 numero: formData.numero,
@@ -391,7 +392,7 @@ const Expedientes = () => {
                 url: formData.url,
                 setOriginalExpedientes
             });
-    
+
             if (success) {
                 toast.info('Se actualizó correctamente el Expediente', {
                     icon: () => <img src={check} alt="Success Icon" />,
@@ -402,15 +403,15 @@ const Expedientes = () => {
             } else {
                 if (error === 'No se pudo obtener la información de la URL proporcionada. Intente de nuevo.') {
                     toast.error('La URL proporcionada es incorrecta. Intente de nuevo.');
-                     
-                }   else if (error === 'Tribunal no funciono') {
+
+                } else if (error === 'Tribunal no funciono') {
                     toast.error('El Tribunal Virtual fallo al devolver los datos. Intente de nuevo.');
-                } 
-                 else {
+                }
+                else {
                     toast.error('Algo mal sucedió al actualizar el Expediente: ' + error);
                 }
             }
-    
+
         } catch (error) {
             console.error(error);
             toast.error('Algo mal sucedió al actualizar el Expediente');
@@ -419,7 +420,7 @@ const Expedientes = () => {
             setIsLoading(false);
         }
     };
-    
+
 
     const openModalDelete = (expediente) => {
         setFormData({
@@ -715,6 +716,14 @@ const Expedientes = () => {
                                         multiple
                                     />
                                 </div>
+                                <div className="flex flex-col">
+                                    <div className="flex">
+                                        <input type="checkbox" id="acceptMessages" className="mr-2" />
+                                        <label htmlFor="acceptMessages">
+                                            Actualizar Expedientes
+                                        </label>
+                                    </div>
+                                </div>
                                 <div className="flex justify-end mt-4 w-full">
                                     <button
                                         disabled={isLoading || !areFilesValid}
@@ -724,6 +733,7 @@ const Expedientes = () => {
                                         Subir Archivos
                                     </button>
                                 </div>
+                               
                             </div>
                         </div>
                     </div>
@@ -839,7 +849,7 @@ const Expedientes = () => {
                             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
                                 Actualizar Expediente
                             </h3>
-                            <button onClick={closeModalUpdate} disabled={isLoading}  type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
+                            <button onClick={closeModalUpdate} disabled={isLoading} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
                                 <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                                 </svg>
@@ -925,18 +935,18 @@ const Expedientes = () => {
 
 
                             <div>
-                                 <button
+                                <button
                                     disabled={isLoading}
                                     type="submit"
                                     className="w-full mt-4 rounded border border-primary bg-primary p-3 text-white transition hover:bg-opacity-90"
                                 >
                                     {isLoading ? (
                                         <div role="status">
-                                            <svg aria-hidden="true" class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-primary" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-primary" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
                                                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="white" />
                                             </svg>
-                                            <span class="sr-only">Actualizando...</span>
+                                            <span className="sr-only">Actualizando...</span>
                                         </div>
                                     ) : (
                                         'Actualizar'
@@ -951,17 +961,17 @@ const Expedientes = () => {
             <div className="absolute top-0 left-0 z-0 w-full h-full"></div>
 
 
-            <div id="popup-modal" tabindex="-1" class={`${isModalOpenDelete ? '' : 'hidden'} fixed top-0 right-0 left-0 bottom-0 flex justify-center items-center bg-black bg-opacity-50 z-50`}>
-                <div class="bg-white rounded-lg shadow w-full max-w-md">
-                    <div class="p-4 md:p-5 text-center">
-                        <svg class="mx-auto mb-4 text-gray-400 w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            <div id="popup-modal" tabIndex="-1" className={`${isModalOpenDelete ? '' : 'hidden'} fixed top-0 right-0 left-0 bottom-0 flex justify-center items-center bg-black bg-opacity-50 z-50`}>
+                <div className="bg-white rounded-lg shadow w-full max-w-md">
+                    <div className="p-4 md:p-5 text-center">
+                        <svg className="mx-auto mb-4 text-gray-400 w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                         </svg>
-                        <h3 class="mb-5 text-lg font-normal text-gray-500">Esta seguro que quieres eliminar este Expediente??</h3>
-                        <button onClick={handleDelete} type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-3">
+                        <h3 className="mb-5 text-lg font-normal text-gray-500">Esta seguro que quieres eliminar este Expediente??</h3>
+                        <button onClick={handleDelete} type="button" className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-3">
                             {isDeleting ? <Spinner size='sm' color="default" /> : 'Si, estoy seguro'}
                         </button>
-                        <button  onClick={closeModalDelete} disabled={isDeleting} type="button" class="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary focus:z-10 focus:ring-4 focus:ring-gray-100">No, cancelar</button>
+                        <button onClick={closeModalDelete} disabled={isDeleting} type="button" className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary focus:z-10 focus:ring-4 focus:ring-gray-100">No, cancelar</button>
                     </div>
                 </div>
             </div>
--- src/components/ExpedienteSial/ExpedienteSial.jsx
+++ src/components/ExpedienteSial/ExpedienteSial.jsx
@@ -372,6 +372,7 @@ const ExpedientesSial = () => {
                                     </button>
                                 </div>
                             </div>
+                            
                         </div>
                     </div>
                 </div>

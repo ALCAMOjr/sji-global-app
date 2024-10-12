da452826664cef8bdd00826fe4c1ed30925a196c
danielaestefania19 <danielacastellano1000@gmail.com>
2024-10-12 17:53:23 -0600

    Component Demanda

---
+++ src/components/Demandas/TableDemanda.jsx
@@ -0,0 +1,21 @@
+ {/* <div className="overflow-x-auto">
+        <table className="min-w-full border-collapse table-auto">
+          <thead>
+            <tr className="bg-blue-500 text-white">
+              <th className="border px-4 py-2">No.</th>
+              <th className="border px-4 py-2">Crédito</th>
+              <th className="border px-4 py-2">Acreditado</th>
+              <th className="border px-4 py-2">Escritura</th>
+              <th className="border px-4 py-2">Fecha Escritura</th>
+              <th className="border px-4 py-2">Inscripción</th>
+              <th className="border px-4 py-2">Volumen</th>
+              <th className="border px-4 py-2">Libro</th>
+              <th className="border px-4 py-2">Propiedad</th>
+              {/* Agrega más columnas según sea necesario 
+            </tr>
+          </thead>
+          <tbody>
+
+          </tbody>
+        </table>
+      </div> */}
